9fe6cf118e125476a7bf619f8f1d20b7ec930e28
isanmartin <isanmartin@ces.com.uy>
2026-07-20 17:06:44 -0300

    Unificar estructura en /tecnoshopper y evitar duplicados

--- atdm-automation-flutter-main/tecnoshopper/android/app/src/androidTest/kotlin/com/example/flutter_ces/AllurePatrolJUnitRunner.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_ces
-
-import pl.leancode.patrol.PatrolJUnitRunner
-
-class AllurePatrolJUnitRunner : PatrolJUnitRunner()
--- atdm-automation-flutter-main/tecnoshopper/android/app/src/main/kotlin/com/example/flutter_ces/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_ces
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
